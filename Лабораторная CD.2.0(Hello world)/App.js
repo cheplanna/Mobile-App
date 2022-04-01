4bf471a01448b2a09a18bf8faa4f95b3d7d44303
cheplanna <72358616+cheplanna@users.noreply.github.com>
2022-04-01 14:47:17 +0300

    Update App.js

--- Лабораторная CD.2.0(Hello world)/App.js
+++ Лабораторная CD.2.0(Hello world)/App.js
@@ -1,1 +1,30 @@
+import * as React from 'react';
+import { Text, View, StyleSheet,Button } from 'react-native';
+import Constants from 'expo-constants';
+import AssetExample from './components/AssetExample';
+import { Card } from 'react-native-paper';
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Button title='Кнопка'/>
+      <Text style={{textAlign:'center'}}>
+        Hello world!
+      </Text>
+      <Card>
+      
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+
+});
